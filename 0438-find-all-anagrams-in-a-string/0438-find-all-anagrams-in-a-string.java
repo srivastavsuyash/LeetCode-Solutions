00b24a5b35c929d88b8927c0733c8d045e088447
Suyash Srivastava <148178404+srivastavsuyash@users.noreply.github.com>
2026-08-11 00:37:28 +0530

    Time: 10 ms (80.7%), Space: 46.8 MB (59.44%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,21 +1,43 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+        // int n = p.length();
+        // int m = s.length();
+        // List<Integer> ans = new ArrayList<>();
+        // if(n > m){
+        //     return ans;
+        // }
+        // char[] arr1 = p.toCharArray();
+        // Arrays.sort(arr1);
+        // String sortedP = new String(arr1);
+        // for(int i=0; i<=m-n; i++){
+        //     String window = s.substring(i, i+n);
+        //     char[] arr2 = window.toCharArray();
+        //     Arrays.sort(arr2);
+        //     String sortedS = new String(arr2);
+        //     if(sortedP.equals(sortedS)){
+        //         ans.add(i);
+        //     }
+        // }
+        // return ans;
+
+        List<Integer> ans = new ArrayList<>();
         int n = p.length();
         int m = s.length();
-        List<Integer> ans = new ArrayList<>();
         if(n > m){
             return ans;
         }
-        char[] arr1 = p.toCharArray();
-        Arrays.sort(arr1);
-        String sortedP = new String(arr1);
-        for(int i=0; i<=m-n; i++){
-            String window = s.substring(i, i+n);
-            char[] arr2 = window.toCharArray();
-            Arrays.sort(arr2);
-            String sortedS = new String(arr2);
-            if(sortedP.equals(sortedS)){
-                ans.add(i);
+        int[] freqP = new int[26];
+        int[] freqWindow = new int[26];
+        for(char ch : p.toCharArray()){
+            freqP[ch - 'a']++;
+        }
+        for(int i=0; i<m; i++){
+            freqWindow[s.charAt(i) - 'a']++;
+            if(i >= n){
+                freqWindow[s.charAt(i-n) - 'a']--;
+            }
+            if(Arrays.equals(freqP, freqWindow)){
+                ans.add(i-n+1);
             }
         }
         return ans;
